eb513386ed3c269fbea149f49d5c36186f771b40
Andrew Csontos <andrew.csontos@socialtables.com>
2022-03-03 14:15:40 -0500

    first

---
+++ index.ts
@@ -0,0 +1,39 @@
+
+//@ts-ignore - http-error doesn't have types
+import redis from 'redis-promisify';
+
+console.log("connecting")
+const client = redis.createClient(6379, "127.0.0.1");
+console.log("connected")
+
+async function main() {
+
+    const x = await client.setexAsync("andrew", 99, new Date().toLocaleString());
+    
+
+    let output = await client.getAsync("andrew")
+    console.log(output)
+
+    output = await client.zaddAsync("mykey", 3, "junk1")
+    output = await client.zaddAsync("mykey", 2, "junk2")
+    output = await client.zaddAsync("mykey", 1, "junk3")
+    
+    const keys = await client.keysAsync("*");
+    for (const key of keys) {
+        const type = await client.typeAsync(key)
+        const items = type=="zset" ? await client.zcardAsync(key) : null;
+        console.log(`${key} (${type}) ${items}`)
+    };
+    //await sleep(1);
+
+    // console.log('flushing')
+    // await client.flushdbAsync();
+
+  
+    console.log('ending')
+    client.quit()
+    console.log('im done')
+}
+
+main();
+
